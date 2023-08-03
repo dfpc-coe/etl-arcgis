2af06057d4f00934f6ac8707e3df4fda645ed0b0
ingalls <nick@ingalls.ca>
2023-08-02 20:23:01 -0600

    Return Schema

--- task.ts
+++ task.ts
@@ -1,7 +1,9 @@
 import fs from 'node:fs';
 import { FeatureCollection } from 'geojson';
+import { JSONSchema6 } from 'json-schema';
 import ETL, {
-    Event
+    Event,
+    SchemaType
 } from '@tak-ps/etl';
 import EsriDump, {
     EsriDumpConfigInput,
@@ -19,60 +21,70 @@ try {
 }
 
 export default class Task extends ETL {
-    static schema() {
-        return {
-            type: 'object',
-            required: ['ARCGIS_URL'],
-            properties: {
-                'ARCGIS_URL': {
-                    type: 'string',
-                    description: 'ArcGIS MapServer URL to pull data from'
-                },
-                'ARCGIS_HEADERS': {
-                    type: 'array',
-                    description: 'Headers to include in the request',
-                    items: {
-                        type: 'object',
-                        required: [
-                            'key',
-                            'value'
-                        ],
-                        properties: {
-                            key: {
-                                type: 'string'
-                            },
-                            value: {
-                                type: 'string'
+    static async schema(type: SchemaType = SchemaType.Input): Promise<JSONSchema6> {
+        if (type === SchemaType.Input) {
+            return {
+                type: 'object',
+                required: ['ARCGIS_URL'],
+                properties: {
+                    'ARCGIS_URL': {
+                        type: 'string',
+                        description: 'ArcGIS MapServer URL to pull data from'
+                    },
+                    'ARCGIS_HEADERS': {
+                        type: 'array',
+                        description: 'Headers to include in the request',
+                        items: {
+                            type: 'object',
+                            required: [
+                                'key',
+                                'value'
+                            ],
+                            properties: {
+                                key: {
+                                    type: 'string'
+                                },
+                                value: {
+                                    type: 'string'
+                                }
                             }
                         }
-                    }
-                },
-                'ARCGIS_PARAMS': {
-                    type: 'array',
-                    description: 'URL Params to include in the request',
-                    items: {
-                        type: 'object',
-                        required: [
-                            'key',
-                            'value'
-                        ],
-                        properties: {
-                            key: {
-                                type: 'string'
-                            },
-                            value: {
-                                type: 'string'
+                    },
+                    'ARCGIS_PARAMS': {
+                        type: 'array',
+                        description: 'URL Params to include in the request',
+                        items: {
+                            type: 'object',
+                            required: [
+                                'key',
+                                'value'
+                            ],
+                            properties: {
+                                key: {
+                                    type: 'string'
+                                },
+                                value: {
+                                    type: 'string'
+                                }
                             }
                         }
+                    },
+                    'DEBUG': {
+                        type: 'boolean',
+                        default: false,
+                        description: 'Print ADSBX results in logs'
                     }
-                },
-                'DEBUG': {
-                    type: 'boolean',
-                    default: false,
-                    description: 'Print ADSBX results in logs'
                 }
-            }
-        };
+            };
+        } else {
+            const task = new Task();
+            const layer = await task.layer();
+            const dumper = new EsriDump(String(layer.environment.ARCGIS_URL));
+            const schema = await dumper.schema();
+
+            console.error(JSON.stringify(schema));
+            return schema;
+        }
     }
 
     async control(): Promise<void> {
@@ -82,6 +94,9 @@ export default class Task extends ETL {
 
         if (!layer.environment.ARCGIS_HEADERS) layer.environment.ARCGIS_HEADERS = [];
         if (!layer.environment.ARCGIS_PARAMS) layer.environment.ARCGIS_PARAMS = [];
+
+        if (!Array.isArray(layer.environment.ARCGIS_HEADERS)) throw new Error('ArcGIS_HEADERS must be an Array');
+        if (!Array.isArray(layer.environment.ARCGIS_PARAMS)) throw new Error('ArcGIS_PARAMS must be an Array');
 
         const config: EsriDumpConfigInput = {
             approach: EsriDumpConfigApproach.ITER,
@@ -98,7 +113,7 @@ export default class Task extends ETL {
             config.headers[param.name] = param.value || '';
         }
 
-        const dumper = new EsriDump(layer.environment.ARCGIS_URL, config);
+        const dumper = new EsriDump(String(layer.environment.ARCGIS_URL), config);
 
         dumper.fetch();
 
@@ -122,8 +137,11 @@ export default class Task extends ETL {
 }
 
 export async function handler(event: Event = {}) {
-    if (event.type === 'schema') {
-        return Task.schema();
+    event.type = 'schema:output';
+    if (event.type === 'schema:input') {
+        return Task.schema(SchemaType.Input);
+    } else if (event.type === 'schema:output') {
+        return Task.schema(SchemaType.Output);
     } else {
         const task = new Task();
         await task.control();
